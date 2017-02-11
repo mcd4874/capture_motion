e213e7348c99c7e937c0a10267fd21c9c6ed9b15
Jesse Cochran <jcochran800@gmail.com>
2017-02-11 17:32:49 -0500

    Successful POST

--- photo.py
+++ photo.py
@@ -14,25 +14,26 @@ cloudinary.config(
 )
 
 #Save json result in result
-result = cloudinary.uploader.upload("/Users/jessecochran/Documents/brickhack/capture_motion/a.jpg")
+result = cloudinary.uploader.upload("/Users/jessecochran/Documents/brickhack/capture_motion/test.jpg")
 
 
-url =  result['url']
+url = str(result['url'])
+image_url = {"url": url}
 
 headers = {
-    #Request headers
-    'Content-Type': 'application/son',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '9af8ddbf990f47b79733d388e18299ab',
 }
+print(image_url)
+params = urllib.urlencode({
+})
 
-params = urllib.urlencode ({})
-
-try:
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{body}", headers)
-    respose = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print "err"
+conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+conn.request("POST", "/emotion/v1.0/recognize?%s", str(image_url), headers)
+response = conn.getresponse()
+data = response.read()
+print(data)
+conn.close()
+#except Exception as e:
+   # print("[Errno {0}] {1}".format(e.errno, e.strerror))
