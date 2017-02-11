7303dc3fc62e28fe347da0c201050f3d97a2145f
Jesse Cochran <jcochran800@gmail.com>
2017-02-11 15:29:12 -0500

    Ready for Microsoft Key

--- photo.py
+++ photo.py
@@ -1,13 +1,38 @@
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import json
+import httplib
+import urllib
+import base64
 
+#Configure cloudinary
 cloudinary.config(
     cloud_name="dwsxmzrpt",
     api_key="387981548284158",
     api_secret="o-lfpbw-L0mnBnC4B_iVBeV5tzU"
 )
 
-result = cloudinary.uploader.upload()
+#Save json result in result
+result = cloudinary.uploader.upload("/Users/jessecochran/Documents/brickhack/capture_motion/a.jpg")
 
 
+url =  result['url']
+
+headers = {
+    #Request headers
+    'Content-Type': 'application/son',
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.urlencode ({})
+
+try:
+    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{body}", headers)
+    respose = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print "err"
